f9cbc6719d0ac024ce48e5838f2c8b7d652b84dc
sharanjeetkaur27733-lang <sharanjeetkaur27733@gmail.com>
2026-03-18 20:17:46 +0530

    commit

--- app/src/main/java/com/example/collegeservices/HomeFragment.kt
+++ app/src/main/java/com/example/collegeservices/HomeFragment.kt
@@ -5,28 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
 
 class HomeFragment : Fragment() {
-
-    private lateinit var startBtn: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-
-        // Connect button
-        startBtn = view.findViewById(R.id.startBtn)
-
-        // Button click
-        startBtn.setOnClickListener {
-            Toast.makeText(requireContext(), "Welcome to College App", Toast.LENGTH_SHORT).show()
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
